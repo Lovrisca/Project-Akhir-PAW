35604d2d0f05958b8c4a4209880f9bc81c21240a
Lovrisca <lovrisca@student.ub.ac.id>
2022-12-06 13:20:06 +0700

    02

--- projectpaw/resources/views/dashboard.blade.php
+++ projectpaw/resources/views/dashboard.blade.php
@@ -177,14 +177,20 @@
 
     <!-- FOOTER -->
     <footer>
+    @foreach ($cafes as $c)
         <div class="container" id="footer">
             <div class="footer-content">
                 <div class="address">
                     <a href="{{ url('/') }}"><img src="{{ url('images/maps.png') }}" alt=""></a>
-                    <p>Address</p>
+                    <p>{{ $c->alamat_cafe }}</p>
+                </div>&emsp;&emsp;
+                <div class="address">
+                    <a href="{{ url('/') }}"><img src="{{ url('images/Call.png') }}" alt=""></a>
+                    <p>{{ $c->no_telepon }}</p>
                 </div>
             </div>
         </div>
+        @endforeach
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
